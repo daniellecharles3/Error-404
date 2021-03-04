83b84672b10f00648877abdee91b5b310679aa74
TriniMisery <36904042+TriniMisery@users.noreply.github.com>
2021-03-03 23:20:30 -0400

    latest version

--- openFile.py
+++ openFile.py
@@ -1,28 +1,42 @@
 import os
 import sys
+import io
+import zipfile
+import shutil
 
-walk_dir = sys.argv[0]
 
-print('walk_dir = ' + walk_dir)
-
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
-
-for root, subdirs, files in os.walk(walk_dir):
-    print('--\nroot = ' + root)
-    list_file_path = os.path.join(root, 'hello_world.py')
-    print('list_file_path = ' + list_file_path)
-
-    with open(list_file_path, 'wb') as list_file:
-        for subdir in subdirs:
-            print('\t- subdirectory ' + subdir)
-
-        for filename in files:
-            file_path = os.path.join(root, filename)
-
-            print('\t- file %s (full path: %s)' % (filename, file_path))
-
-            with open(file_path, 'rb') as f:
-                f_content = f.read()
-                list_file.write(('The file %s contains:\n' % filename).encode('utf-8'))
-                list_file.write(f_content)
-                list_file.write(b'\n')
+def extract(filename):
+    z = zipfile.ZipFile(filename)
+    for f in z.namelist():
+        # get directory name from file
+        dirname = os.path.splitext(f)[0]  
+        # create new directory
+        os.mkdir(dirname)  
+        # read inner zip file into bytes buffer 
+        content = io.BytesIO(z.read(f))
+        zip_file = zipfile.ZipFile(content)
+        
+        file_path = ''# remembers entire file address of last file found per iteration 
+        filename = ''# remembers name of last file found per iteration 
+        cwd = os.getcwd()
+        dir = os.path.join("C:\\",cwd,"Programs")# creates new folder 
+        if not os.path.exists(dir): # makes sure that the folder is not made multiple times
+            os.mkdir(dir)
+        for file in zip_file.namelist():# this iterates every directory found in the namelist buffer
+            
+            for root, subdirs, files in os.walk(dirname):# this is how os.walk is used. 
+                name = ''
+                for files in file: #for some reason the files are found in characters so this loop iterates the name of the files found and updates the name sring variabe 
+                    name += files
+                    file_path = os.path.join(root, name)    
+                    filename = name
+                    
+            zip_file.extract(file, dirname)
+            #this code was suppose to move the directory of the files found to the new folder created
+            #os.rename(os.path.abspath(name), os.path.join(dir, name))
+            
+        print('walk_dir (absolute) = ' + os.path.abspath(filename)) # just a test print statement showing file addresses
+        print ('--------------------------------')  
+    
+#run the function here   
+extract('1234.zip')
