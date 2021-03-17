fd90b0c8b32c799dae60f9d30ebfa706ba7a4a3f
TriniMisery <36904042+TriniMisery@users.noreply.github.com>
2021-03-17 00:15:36 -0400

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,64 @@
+import sys, os
+from PyQt5.QtWidgets import QMainWindow, QAction, QMenu, QApplication, QFileDialog 
+import subprocess
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self): 
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        impMenu = QMenu('Import', self)
+        impAct = QAction('Import mail', self)
+        impMenu.addAction(impAct)
+
+        newAct = QAction('New', self)
+        uploadAct = QAction('Upload', self)
+        uploadAct.triggered.connect(self.openFileNameDialog)
+        
+
+
+        fileMenu.addAction(newAct)
+        fileMenu.addAction(uploadAct)
+                
+        fileMenu.addMenu(impMenu) 
+        
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Submenu')
+        self.show()
+    
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+    
+    def openFileNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+        if files:
+            print(files)
+    
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            print(fileName)
+  
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
